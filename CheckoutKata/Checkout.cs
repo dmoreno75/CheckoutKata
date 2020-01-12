20f38e15bff7f3f7fe6adf3c41868c5043c68708
Greibach <david.moreno.uk@gmail.com>
2020-01-12 12:57:36 +0000

    Add project files.

---
+++ CheckoutKata/Checkout.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Kata
+{
+	public class Checkout
+	{
+		public decimal Total()
+		{
+			return 0m;
+		}
+
+		public void Scan(Item item)
+		{
+
+		}
+
+	}
+	public class Item
+	{
+		public Item()
+		{
+
+		}
+	}
+}
