4ba9fab6117c27d3cec4bd913bc0d7bee8616668
Greibach <david.moreno.uk@gmail.com>
2020-01-12 14:12:56 +0000

    Initial tests

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -4,22 +4,47 @@ namespace Kata
 {
 	public class Checkout
 	{
+
+		decimal total = 0m;
+
 		public decimal Total()
 		{
-			return 0m;
+			return total;
 		}
 
 		public void Scan(Item item)
 		{
-
+			total += item.UnitPrice;
 		}
 
 	}
 	public class Item
 	{
-		public Item()
+		public string SKU { get; set; }
+		public decimal UnitPrice { get; set; }
+		public Item(string sku, decimal unitPrice)
 		{
-
+			this.SKU = sku;
+			this.UnitPrice = unitPrice;
+		}
+		public Item(string sku)
+		{
+			this.SKU = sku;
+			this.UnitPrice = 0;
 		}
 	}
+
+	public class SpecialOffer
+	{
+		public string SKU { get; set; }
+		public uint Quantity { get; set; }
+		public decimal OfferPrice { get; set; }
+		public SpecialOffer(string sku, uint quantity, decimal offerPrice)
+		{
+			this.SKU = sku;
+			this.Quantity = quantity;
+			this.OfferPrice = offerPrice;
+		}
+	}
+
 }
--- CheckoutKataTests/CheckoutTests.cs
+++ CheckoutKataTests/CheckoutTests.cs
@@ -1,18 +1,47 @@
+using Kata;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Kata.Tests
 {
 	public class CheckoutTests
 	{
+		List<Item> testProductCatalogue = new List<Item>
+		{
+			new Item("A99", 0.50m),
+			new Item("B15", 0.30m),
+			new Item("C40", 0.60m),
+		};
+
+		List<SpecialOffer> testSpecialOffers = new List<SpecialOffer>
+		{
+			new SpecialOffer("A99", 3, 1.30m),
+			new SpecialOffer("B15", 2, 0.45m),
+		};
+
 		[SetUp]
 		public void Setup()
 		{
 		}
 
 		[Test]
-		public void Test1()
+		public void WhenEmptyBasket_ExpectedTotalZero()
 		{
-			Assert.Pass();
+			var checkout = new Checkout();
+
+			Assert.AreEqual(checkout.Total(), 0);
+		}
+
+		[Test]
+		public void WhenScanSingleItem_ExpectedTotalCorrect()
+		{
+			var checkout = new Checkout();
+
+			var item = new Item("A99", 0.50m);
+
+			checkout.Scan(item);
+
+			Assert.AreEqual(checkout.Total(), 0.50m);
 		}
 	}
 }
